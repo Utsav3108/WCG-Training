a99c6283fd9ea6039e3f0f6ebe3a7269203c57d8
utsav3108 <pandyautsav3108@gmail.com>
2024-06-18 12:23:47 +0530

    #1 usermanagement file creation

---
+++ WCGTrainingGit/UserManagement/usermanagement.swift
@@ -0,0 +1,14 @@
+//
+//  usermanagement.swift
+//  WCGTrainingGit
+//
+//  Created by webcodegenie on 18/06/24.
+//
+
+import Foundation
+
+class UserManagement{
+    func printUsers(){
+        print("this is user!")
+    }
+}
